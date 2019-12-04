3e98fac0ea51fe9da9af8a52ee0cf993c420644a
Gilberto77 <salemcito@outlook.es>
2019-12-03 20:33:24 -0600

    Todo bien se modifica BussinesTypes

---
+++ ElMercadito.Web/Controllers/API/BusinessTypesController.cs
@@ -0,0 +1,29 @@
+﻿using ElMercadito.Web.Data;
+using ElMercadito.Web.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ElMercadito.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BusinessTypesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public BusinessTypesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/BusinessTypes
+        [HttpGet]
+        public IEnumerable<BusinessType> GetBusinessTypes()
+        {
+            return _context.BusinessTypes.OrderBy(bt => bt.Name);
+        }
+
+
+    }
+}
